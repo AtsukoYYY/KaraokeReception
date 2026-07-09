2683f3d8f8ba176b4d363bcf574b5ec320bc37cf
A.Yamada <bigdipper6t@gmail.com>
2026-07-09 20:39:22 +0900

    1st Commit

---
+++ KaraokeReception/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KaraokeReception.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
